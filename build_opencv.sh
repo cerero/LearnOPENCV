e056a142f32299232efee7656d32882b2d13e30a
yongjiewang <cerero111@gmail.com>
2019-02-13 22:25:03 +0800

    Cr:保存一下mac下的编译用信息

--- build_opencv.sh
+++ build_opencv.sh
@@ -10,3 +10,17 @@ mkdir release
 cmake -S ../opencv_source -B ./build -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=./release/
 
 
+# mac下链接openCV的一些参数
+# linker
+# -stdlib=libc++
+#
+# opencv_highgui
+# opencv_imgcodecs
+# opencv_imgproc
+# opencv_core
+#
+# compile
+# -c -fmessage-length=0 -std=c++11
+#
+# Build step
+# install_name_tool -add_rpath /Users/lisujia/workspace/LearnOPENCV/opencv_lib/release/lib ./TestOpenCV
